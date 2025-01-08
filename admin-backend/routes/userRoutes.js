0c8dbfab2c1bbc30397fa309291a96b0b1420c84
sabine-aa <sabineabouammar1@gmail.com>
2025-01-08 09:52:01 +0200

    updated routes

--- admin-backend/routes/userRoutes.js
+++ admin-backend/routes/userRoutes.js
@@ -1,10 +1,28 @@
+// import express from "express";
+// import authMiddleware from "../middlewares/authMiddleware.js";
+
+// const router = express.Router();
+
+// router.get("/protected", authMiddleware, (req, res) => {
+//   res.status(200).json({ message: "Access granted", user: req.user });
+// });
+
+// export default router;
 import express from "express";
-import authMiddleware from "../middlewares/authMiddleware.js";
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/protected", authMiddleware, (req, res) => {
-  res.status(200).json({ message: "Access granted", user: req.user });
-});
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
